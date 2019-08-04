9fa25c74a3daa210cb3be716e19ec64a278c8f3a
xzj963179571 <xiaozhenjie5221@163.com>
2019-08-04 11:06:16 +0800

    selenium

--- python_demo/python_selenium_01/python_selenium_06.py
+++ python_demo/python_selenium_01/python_selenium_06.py
@@ -97,30 +97,34 @@ import unittest
 import HTMLTestRunner
 import time
 
-class XdclassTestCase(unittest.TestCase):
+class AUTO_Test(unittest.TestCase):
     def setUp(self):
         self.age = 32
         self.name = "小D课堂"
         print("setup  method")
 
     def test_one(self):
+        u"test_one方法"
         print("test_one 二当家小D来了")
         # 断言是否相等
         self.assertEqual(self.name,"小D课堂",msg="名字不对")
 
 
     def test_two(self):
+        u"test_two方法"
         print("test_two 前端来了")
         # 断言是否为true，msg是断言错误的提示信息
         self.assertTrue('XD'.isupper(),msg="不是大写")
 
 
     def test_three(self):
+        u"test_three方法"
         print("test_three 后端来了")
         self.assertEqual(self.age,32)
 
 
     def test_four(self):
+        u"test_four方法"  #案例title说明
         print("test_four 小D课堂官网上线啦 https://www.xdclass.net")
         self.assertEqual(self.age,32)
 
@@ -130,22 +134,29 @@ class XdclassTestCase(unittest.TestCase):
         self.assertEqual('foo'.upper(),'FOO')
 
 
+
+
+
+
+
 if __name__ == '__main__':
     suite = unittest.TestSuite()
-    suite.addTest(XdclassTestCase('test_one'))
-    suite.addTest(XdclassTestCase('test_two'))
-    suite.addTest(XdclassTestCase('test_three'))
-    suite.addTest(XdclassTestCase('test_four'))
+    suite.addTest(AUTO_Test('test_one'))
+    suite.addTest(AUTO_Test('test_two'))
+    suite.addTest(AUTO_Test('test_three'))
+    suite.addTest(AUTO_Test('test_four'))
     # verbosity参数可以控制执行结果的输出，0是简单报告，1是一般报告（默认），2是详细报告
     # runner = unittest.TextTestRunner(verbosity=2)
     # runner.run(suite)
-    file_prefix = time.strftime("%Y.%m.%d %H_%M_%S",time.localtime())
-    print(file_prefix)
 
+    #
     # 创建测试报告，此时这个文件还是空文件
     # wb 以二进制格式打开一个文件，只用于写入，如果文件存在则覆盖，不存在则创建
-    fp = open("D:/"+file_prefix+"_result.html","wb")
+    file_prefix = time.strftime("%Y.%m.%d %H_%M_%S", time.localtime())
+    print(file_prefix)
+    file_name = "D:\\log\\result.html"
+    fp = open(file_name,"wb")
     # stream  定义一个测试报告写入的文件，title就是标题，description就是描述
-    runner = HTMLTestRunner.HTMLTestunner(stream=fp,title=u"小D课堂 测试报告",description=u"c测试用例执行情况")
+    runner = HTMLTestRunner.HTMLTestRunner(stream=fp,title=u"小D课堂 测试报告",description=u"c测试用例执行情况")
     runner.run(suite)
     fp.close()
